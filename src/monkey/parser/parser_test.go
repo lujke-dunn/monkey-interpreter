55e862b374d995751986182f0b13133bc7594e5d
Luke Dunn <lukedunn011@gmail.com>
2024-12-15 15:22:04 +1100

    finished letStmt testcase

--- src/monkey/parser/parser_test.go
+++ src/monkey/parser/parser_test.go
@@ -25,18 +25,44 @@ func TestLetStatements(t *testing.T) {
 		t.Fatalf("program.Statements does not contain 3 statements got=%d", len(program.Statements))
 	}
 
-	test := []struct {
+	tests := []struct {
 		expectedIdentifier string
 	}{
-		{x},
-		{y},
+		{"x"},
+		{"y"},
 		{"foobar"},
 	}
 
 	for i, tt := range tests { 
 		smts := program.Statements[i]
-		
+		if !testLetStatements(t, smts, tt.expectedIdentifier) {
+			return
+		}
 	}
+}
+
+	func testLetStatements(t *testing.T, s ast.Statement, name string) bool {
+		if s.TokenLiteral() != "let" {
+			t.Errorf("s.TokenLiteral does not equal let got%T=", s)
+			return false
+		}
 	
 
-}
+		letStmt, ok := s.(*ast.LetStatement) 
+		if !ok {
+			t.Errorf("s not *ast.LetStatement. got=%T", s)
+			return false
+		}
+		
+
+		if letStmt.Name.Value != name {
+			t.Errorf("letStmt.Name.Value not '%s', got=%s", name, letStmt.Name.Value)
+			return false
+		}
+
+		if letStmt.Name.TokenLiteral() != name {
+			t.Errorf("letStmt.Name.TokenLiteral() not '%s'. got=%s", name, letStmt.Name.TokenLiteral())
+		}
+
+		return true
+	}
