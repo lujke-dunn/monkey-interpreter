0f13317ce03388961eba4b8c8d16c69aa2b81fba
Luke Dunn <lukedunn011@gmail.com>
2024-12-29 10:11:52 +1100

    added a testcase for prefixExpression

--- src/monkey/parser/parser_test.go
+++ src/monkey/parser/parser_test.go
@@ -75,6 +75,44 @@ func TestIntegerLiteralExpression(t *testing.T) {
 
 }
 
+func TestParsingPrefixExpressions(t *testing.T) {
+	prefixTests :=  []struct {
+		input    	 string
+		operator 	 string
+		integerValue int64
+	}{
+		{"!5", "!", 5},
+		{"-15", "-", 15},
+	}
+
+	for _, tt := range prefixTests {
+		l := lexer.New(tt.input)
+		p := New(l)
+		program := p.ParseProgram()
+		checkParserErrors(t, p)
+
+		if len(program.Statements) != 1 { 
+			t.Fatalf("program.Statements does not contain 1 statement got=%d", len(program.Statements))
+		}
+
+		stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
+		if !ok {
+			t.Fatalf("program.Statement[0] is not ast.ExpressionStatment got=%T", program.Statements[0])
+		}
+
+		exp, ok := program.Statements[0].(*ast.PrefixExpression) 
+		if !ok {
+			t.Fatalf("stmt is not ast.PrefixExpression. got=%T", program.Statements[0])
+		}
+
+		if exp.Operator != tt.operator {
+			t.Fatalf("exp.Operator is not %s got=%s", tt.operator, exp.Operator)
+		}
+		if !testIntegerLiteral(t, exp.Right, tt.integerValue) {
+			return
+		}
+	}
+}
 
 func TestIdentifierExpression(t *testing.T) { 
 	input := "foobar;"
