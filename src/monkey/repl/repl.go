030a64f4d540f981ca16010f729c61d110d95e46
Luke Dunn <lukedunn011@gmail.com>
2025-01-12 18:05:49 +1100

    repl updated to parser

--- src/monkey/repl/repl.go
+++ src/monkey/repl/repl.go
@@ -1,12 +1,11 @@
 package repl
 
-
 import (
 	"bufio"
 	"fmt"
 	"io"
-	"monkey/token"
 	"monkey/lexer"
+	"monkey/parser"
 )
 
 const PROMPT = ">>"
@@ -23,8 +22,22 @@ func Start(in io.Reader, out io.Writer) {
 
 		line := scanner.Text()
 		l := lexer.New(line)
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Fprintf(out, "%+v\n", tok)
+		p := parser.New(l)
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+
+func printParserErrors(out io.Writer, errors []string) {
+	for _, msg := range errors {
+		io.WriteString(out, "Monkey find error!\n")
+		io.WriteString(out, " \t"+msg+"\n")
 	}
 }
