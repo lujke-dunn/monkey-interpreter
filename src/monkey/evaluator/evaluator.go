6769c8e9660b996edf5c979b7f44e19f90fc98a3
Luke Dunn <lukedunn011@gmail.com>
2025-01-13 13:24:05 +1100

    prefix expressions added; bang and minus; testcoverage included

--- src/monkey/evaluator/evaluator.go
+++ src/monkey/evaluator/evaluator.go
@@ -5,6 +5,11 @@ import (
 	"monkey/object"
 )
 
+var (
+	NULL = &object.NULL{}
+	TRUE = &object.Boolean{Value: true}
+	FALSE = &object.Boolean{Value: false}
+)
 
 func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
@@ -19,9 +24,57 @@ func Eval(node ast.Node) object.Object {
 	// Expressions
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
+	case *ast.Boolean:
+		return nativeBoolToBooleanObject(node.Value)
+	case *ast.PrefixExpression:
+		right := Eval(node.Right)
+		return evalPrefixExpression(node.Operator, right)
 	}
 
 	return nil 
+}
+
+
+func evalPrefixExpression(operator string, right object.Object) object.Object {
+	switch operator {
+		case "!":
+			return evalBangOperatorExpression(right) 
+		case "-":
+			return evalMinusPrefixOperatorExpression(right)
+		default:
+			return NULL
+	}
+}
+
+
+func evalMinusPrefixOperatorExpression(right object.Object) object.Object {
+	if right.Type() != object.INTEGER_OBJ {
+		return NULL
+	}
+
+	value := right.(*object.Integer).Value
+	return &object.Integer{Value: -value}
+
+}
+
+func evalBangOperatorExpression(right object.Object) object.Object {
+	switch right {
+		case TRUE:
+			return FALSE
+		case FALSE:
+			return TRUE
+		case NULL:
+			return TRUE
+		default:
+			return FALSE
+	}
+}
+
+func nativeBoolToBooleanObject(input bool) *object.Boolean {
+	if input {
+		return TRUE
+	}
+	return FALSE
 }
 
 
--- src/monkey/evaluator/evaluator_test.go
+++ src/monkey/evaluator/evaluator_test.go
@@ -15,6 +15,8 @@ func TestEvalIntegerExpression(t *testing.T) {
 	}{
 		{"5", 5}, 
 		{"10", 10},
+		{"-5", -5},
+		{"-10", -10},
 	}
 
 	for _, tt := range tests {
@@ -23,7 +25,41 @@ func TestEvalIntegerExpression(t *testing.T) {
 	}
 }
 
+func TestEvalBooleanExpression(t *testing.T) {
+	tests := []struct {
+		input string
+		expected bool
+	}{
+		{"true", true},
+		{"false", false},
+	}
 
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		testBooleanObject(t, evaluated, tt.expected)
+	}
+}
+
+
+func TestBangOperator(t *testing.T) {
+	tests := []struct {
+		input string
+		expected bool
+	}{
+		{"!true", false},
+		{"!!true", true},
+		{"!false", true},
+		{"!!false", false},
+		{"!5", false}, 
+		{"!!5", true}, 
+	}
+
+	for _, tt := range tests {
+		evaluator := testEval(tt.input)
+		testBooleanObject(t, evaluator, tt.expected)
+	}
+}
+ 
 
 func testEval(input string) object.Object { 
 	l := lexer.New(input)
@@ -37,12 +73,28 @@ func testIntegerObject(t *testing.T, obj object.Object, expected int64) bool {
 	result, ok := obj.(*object.Integer)
 
 	if !ok {
-		t.Errorf("object is not Intger. got=%T (%+v)", obj, obj)
+		t.Errorf("object is not Integer. got=%T (%+v)", obj, obj)
 		return false
 	}
 
 	if result.Value != expected {
 		t.Errorf("object has wrong value. got=%d, want=%d", result.Value, expected)
+		return false
+	}
+
+	return true
+}
+
+func testBooleanObject(t *testing.T, obj object.Object, expected bool) bool {
+	result, ok := obj.(*object.Boolean)
+
+	if !ok {
+		t.Errorf("object is not Boolean. got=%T (%+v)", obj, obj)
+		return false
+	}
+
+	if result.Value != expected {
+		t.Errorf("object has wrong value. got=%t, want=%t", result.Value, expected)
 		return false
 	}
 
