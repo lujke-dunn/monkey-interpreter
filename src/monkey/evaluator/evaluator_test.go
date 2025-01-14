15c957e1a97a6c2efa65b9f477fb8e67d5130fd4
Luke Dunn <lukedunn011@gmail.com>
2025-01-14 16:49:30 +1100

    added error object, added error test cases

--- src/monkey/evaluator/evaluator_test.go
+++ src/monkey/evaluator/evaluator_test.go
@@ -23,6 +23,30 @@ func TestReturnStatements(t *testing.T) {
 	}
 }
 
+func TestErrorHandling(t *testing.T) {
+	tests := []struct {
+		input string
+		expectedMessage string
+	}{
+
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+
+		errObj, ok := evaluated.(*object.Error)
+		if !ok {
+			t.Errorf("no error object returned. got=%T(%+v)", evaluated, evaluated)
+			continue
+		}
+
+		if errObj.Message != tt.expectedMessage {
+			t.Errorf("wrong error message. expected=%q, got=%q", tt.expectedMessage, errObj.Message)
+		}
+
+	}
+}
+
 
 func TestEvalIntegerExpression(t *testing.T) {
 	tests := []struct {
