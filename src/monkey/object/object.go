e6d49ccc86942ad05b4a41b78d6b1836cb58bea3
Luke Dunn <lukedunn011@gmail.com>
2025-01-15 16:02:40 +1100

    added function body object and created atest case for it

--- src/monkey/object/object.go
+++ src/monkey/object/object.go
@@ -1,6 +1,11 @@
 package object
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+	"monkey/ast"
+	"strings"
+)
 
 const (
 	INTEGER_OBJ = "INTEGER"
@@ -8,6 +13,7 @@ const (
 	NULL_OBJ = "NULL"
 	RETURN_VALUE_OBJ = "RETURN_VALUE"
 	ERROR_OBJ = "ERROR"
+	FUNCTION_OBJ = "FUNCTION"
 )
 
 type ObjectType string
@@ -78,3 +84,30 @@ func (e *Environment) Set(name string, val Object) Object {
 	return val
 }
 
+
+type Function struct {
+	Parameters []*ast.Identifier
+	Body *ast.BlockStatement
+	Env *Environment
+}
+
+func (f *Function) Type() ObjectType { return FUNCTION_OBJ }
+
+func (f *Function) Inspect() string {
+	var out bytes.Buffer
+
+	params := []string{}
+
+	for _, p := range f.Parameters { 
+		params = append(params, p.String())
+	}
+
+	out.WriteString("fn")
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(") {\n")
+	out.WriteString(f.Body.String())
+	out.WriteString("}")
+
+	return out.String()
+}
