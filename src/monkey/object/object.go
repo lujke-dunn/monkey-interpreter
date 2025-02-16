d7304ea8759cb5aa20bf68f60cc3915923680e1f
lujke-dunn <lukedunn011@gmail.com>
2025-02-16 21:41:24 +1100

    added builtin support:

--- src/monkey/object/object.go
+++ src/monkey/object/object.go
@@ -9,6 +9,7 @@ import (
 
 const (
 	INTEGER_OBJ = "INTEGER"
+	BUILTIN_OBJ = "BUILTIN"
 	BOOLEAN_OBJ = "BOOLEAN"
 	NULL_OBJ = "NULL"
 	RETURN_VALUE_OBJ = "RETURN_VALUE"
@@ -23,6 +24,17 @@ type Object interface {
 	Type() ObjectType
 	Inspect() string
 } 
+
+
+type BuiltinFunction func(args ...Object) Object
+
+type Builtin struct {
+	Fn BuiltinFunction
+}
+
+func (b *Builtin) Type() ObjectType { return BUILTIN_OBJ }
+
+func (b *Builtin) Inspect() string { return "builtin function"}
 
 
 type String struct {
